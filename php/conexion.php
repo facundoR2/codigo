4ddfdd8cfb8b6f1f58f9bb1be92923d689c66b9c
facundoR2 <facundo_rios1@hotmail.com>
2023-09-02 17:43:54 -0300

    se agregaron las paginas

---
+++ php/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "";
+$dbname ="";
+include"config.php";
+include"utils.php";
+
+function conectar($db){
+    try{
+        $conect = new PDO("mysql;host={$db['']}");
+        
+    }
+}
