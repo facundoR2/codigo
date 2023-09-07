69ccfb8ab771bbaa828f787d464f9c2c5dc5a0df
facundoR2 <facundo_rios1@hotmail.com>
2023-09-07 19:21:52 -0300

    se agrego barra de busqueda y boton con label de ingresar, ademas de un php para el login

--- pag principal/dinamic.js
+++ pag principal/dinamic.js
@@ -5,16 +5,29 @@
 // }
 
 //se agregan los listeners para los buttons del menu lateral.
-let nuevologin = document.getElementById("#Blogin");
+let nuevologin = document.getElementById("#Bingresar");
 let Go_toaccesory = document.getElementById("#Baccesorios");
 let Go_tocategory = document.getElementById("#Bcategorias");
 let GO_tofavorites = document.getElementById("#Bfav");
 let Go_tocart = document.getElementById("#Bcarrito");
 let Go_mypc = document.getElementById("#Batp");
 
+//listener para el boton que ingresa ¿
 nuevologin.addEventListener("click",function(){
     Event.preventDefault();
-    win
+    try {
+        irapagLogin();
+
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+    
     
 
 })
+function irapagLogin(){
+    window.location.href="localhost/neutro/codigo/pag login";
+
+}
