d79d93939d826a45a4fb6c5c80b0afd09c57ab9a
facundoR2 <facundo_rios1@hotmail.com>
2023-09-09 16:08:05 -0300

    por fin arregle los botones. pinches punto y coma

--- paglogin/Dlogin.js
+++ paglogin/Dlogin.js
@@ -5,16 +5,26 @@
 // }
 
 //se agregan los listeners para los buttons del menu lateral.
-let Go_login = document.getElementById("botonlogin");
-// var Go_toaccesory = document.getElementById("#Baccesorios");
-// var Go_tocategory = document.getElementById("#Bcategorias");
-// var GO_tofavorites = document.getElementById("#Bfav");
-// var Go_tocart = document.getElementById("#Bcarrito");
-// var Go_mypc = document.getElementById("#Batp");
+var Go_login = document.getElementById("botonlogin");
+var Go_inicio = document.getElementById("btoninicio");
+var Go_accesorios =document.getElementById("btonaccesorios");
+var Go_categorias = document.getElementById("btoncategorias");
+var Go_ATP = document.getElementById("btonATP");
+
 
 //listener para el boton que ingresa ¿
 Go_login.addEventListener("click",function(){
     window.location.href="http://localhost/neutro/codigo/paglogin/loginNeutro/indexlogin.html";
-
 });
-
+Go_inicio.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagPrincipal/index.html";
+})
+Go_accesorios.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagAccesorios/index.html";
+})
+Go_categorias.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagCategorias(buscador)/index.html";
+})
+Go_ATP.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagArmarTuPc/index.html";
+})
