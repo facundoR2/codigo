8d565ead94a5bf8da469b6d830826c80988d5153
facundoR2 <facundo_rios1@hotmail.com>
2023-09-09 19:13:37 -0300

    se cambiarions los href por botones

--- paglogin/loginNeutro/dinamic.js
+++ paglogin/loginNeutro/dinamic.js
@@ -1,21 +1,32 @@
-//se agrega una fuente para mejor calidad visual.
-// FontFace: {
-//     new (family="", source= "" | BinaryData, descriptors? FontFaceDescriptors | undefined): FontFace;
-//     prototype: FontFace;
-// }
+// variables que recolectan los botones para los listener.
+var Go_login = document.getElementById("botonlogin");
+var Go_inicio = document.getElementById("btoninicio");
+var Go_accesorios =document.getElementById("btonaccesorios");
+var Go_categorias = document.getElementById("btoncategorias");
+var Go_ATP = document.getElementById("btonATP");
 
-//se agregan los listeners para los buttons del menu lateral.
-let nuevologin = document.getElementById("#Blogin");
-let Go_toaccesory = document.getElementById("#Baccesorios");
-let Go_tocategory = document.getElementById("#Bcategorias");
-let GO_tofavorites = document.getElementById("#Bfav");
-let Go_tocart = document.getElementById("#Bcarrito");
-let Go_mypc = document.getElementById("#Batp");
+
+//listener para el boton que ingresa ¿
+Go_login.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/paglogin/loginNeutro/indexlogin.html";
+});
+Go_inicio.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagPrincipal/index.html";
+})
+Go_accesorios.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagAccesorios/index.html";
+})
+Go_categorias.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagCategorias(buscador)/index.html";
+})
+Go_ATP.addEventListener("click",function(){
+    window.location.href="http://localhost/neutro/codigo/pagArmarTuPc/index.html";
+})
 
 function login(){
     login.preventDefault();
     // crea variables para recolectar los valores del formulario login
-    var email = document.querySelector("#Ingmail").value;
+    var email = document.querySelector("#IngMail").value;
     var contraseña = document.querySelector("#IngContraseña").value;
     // crea una peticion xmlhttp para hacer una peticion AJAX.
     var Xrequest = new XMLHttpRequest();
