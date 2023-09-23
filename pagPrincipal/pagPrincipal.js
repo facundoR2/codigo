1fb04299484a9a5fb2efd4c44d0ec409acbf36e3
facundoR2 <facundo_rios1@hotmail.com>
2023-09-23 17:56:05 -0300

    cambios

--- pagPrincipal/pagPrincipal.js
+++ pagPrincipal/pagPrincipal.js
@@ -6,8 +6,8 @@
 
 //se agregan los listeners para los buttons del menu lateral.
 let nuevologin = document.getElementById('Bingresar');
-let Go_toaccesory = document.getElementById("bton-accesorios");
-let Go_tocategory = document.getElementById("bton-categorias");
+let BtonAccesorios = document.getElementById("bton-accesorios");
+let BtonCategorias = document.getElementById("bton-categorias");
 let Go_tocart = document.getElementById("bton-carrito");
 let Go_mypc = document.getElementById("bton-ATP");
 let Go_products = document.getElementById("bton-productos");
@@ -27,41 +27,40 @@ function buscarProducto(B_item){
 
 // fin seccion busqueda.
 //seccion funcionalidad session.
-let usuarionombre = document.getElementById("TUsuarioActual");
 function verificarSession(){
-    var usuario = usuarionombre;
-    if (usuario = "cliente"){
+    let usuarionombre = document.getElementById("labelusuario");
+    var usuario = usuarionombre.textContent;
+    console.log(usuario);
+    if (usuario == "cliente"){
         alert("no has ingresado a una session");
         window.location.href="http://localhost/neutro/codigo/paglogin/index.html";
+        var respuesta = false;
+        return respuesta;
     }else{
-        alert("no estas como cliente");
+        alert("estas en una session");
+        var respuesta = true;
+        return respuesta;
     }
 };
 //fin seccion session.
 //listeners de botones para navegar en las paginas.
-nuevologin.addEventListener("click",function(){
-    window.location.href="http://localhost/neutro/codigo/paglogin/index.html";
+BtonCategorias.addEventListener("click",function(){
+    window.location.href="http://localhost/Neutro/codigo/pagCategorias/index.html";
 });
-Go_toaccesory.addEventListener("click",function(){
-    window.location.href="http://localhost/neutro/codigo/pagCategorias(buscador)/index.html";
+BtonAccesorios.addEventListener("click",function(){
+    window.location.href="http://localhost/Neutro/codigo/pagAccesorios/index.html";
 });
 Go_mypc.addEventListener("click",function(){
-    window.location.href="http://localhost/neutro/codigo/pagArmarTuPc/index.html";
-    
-
-});
-Go_tocategory.addEventListener("click",function(){
-
-    window.location.href="http://localhost/neutro/codigo/pagProductos/index.html";
+    window.location.href="http://localhost/Neutro/codigo/pagArmarTuPc/index.html";
 });
 Go_tocart.addEventListener("click",function(){
-    confirmacion = verificarSession(usuarionombre);
-    if (confirmacion = false){
-        console.log(confirmacion);
+    verificarSession()
+    if(verificarSession==true){
+        window.location.href="http://localhost/Neutro/codigo/pagCarrito/index.html";
+    }else{
+        alert("por favor ingresa a una sesion para acceder a un carrito");
     }
-
-
-
 });
+
 
 //verificadores
