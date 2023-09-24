5c12bac55a52cb2c08e95a4c0761d8c8768e47d7
facundoR2 <facundo_rios1@hotmail.com>
2023-09-23 23:41:45 -0300

    se cambio la conexion de PDO a mysqli y se creo registro.php que permite el registro de usuarios.

--- php/conexion.php
+++ php/conexion.php
@@ -1,23 +1,11 @@
 <?php
-$host="localhost";
-$usuario="root";
-$contraseña="";
-$db="datos-local";
 
-try{
-    $conn = new PDO("mysql:host=$host;dbname=$db",$usuario,$contraseña);
-    //establecer el modo error.
-    $conn->setattribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    if(!$conn){
-        echo json_encode("ERROR AL CONECTARSE");
-    }else{
-        echo json_encode("CONEXION EXITOSA");
-    }
-
-}catch(PDOException $e){
-
-    echo json_encode("fallo al conectar a msql:". $e->getMessage());
-}
+$conn = new mysqli("localhost","root","","datos-local");
+    // if(!$conn){
+    //     echo json_encode("ERROR AL CONECTARSE");
+    // }else{
+    //     echo json_encode("CONEXION EXITOSA");
+    // }
 ?>
 
 
