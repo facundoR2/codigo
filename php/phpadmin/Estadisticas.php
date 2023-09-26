a62c97496263c82995436c1e020ac2802cb60423
facundoR2 <facundo_rios1@hotmail.com>
2023-09-26 09:48:52 -0300

    se crearon php para Estadisticas, buscar producto (admin) y buscar producto(cliente).

--- php/phpadmin/Estadisticas.php
+++ php/phpadmin/Estadisticas.php
@@ -1,1 +1,6 @@
 <!-- aqui estaran los movimientos para las estadisticas que accede el admin -->
+<?php
+include("../php/conexion.php");
+
+
+?>
--- php/phpadmin/httpProducto.php
+++ php/phpadmin/httpProducto.php
@@ -1,1 +1,15 @@
 <!-- en este php se incluiran los comandos GET;POST;UPDATE;DELETE fisico y ideal -->
+<?php 
+if [("REQUEST_METHOD") === 'GET']{
+}
+if [("REQUEST_METHOD") === 'POST']{
+
+}
+if [("REQUEST_METHOD") === 'UPDATE']{
+
+}
+if [("REQUEST_METHOD") === 'DELETE']{
+    
+}
+
+?>
