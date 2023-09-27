d51e60921c967825115be6288539b15492c48962
facundoR2 <facundo_rios1@hotmail.com>
2023-09-27 18:10:16 -0300

    se reconfiguro la consulta para para que realice busqueda en "tipo" y "Nombre".

--- php/Buscarproducto-fe-pp1.php
+++ php/Buscarproducto-fe-pp1.php
@@ -6,39 +6,35 @@ include ("conexion.php");
 //recibimos el parametro de busqueda de la barra buscador.
 
 $SEARCH = $_POST["Busqueda"];
+// $SEARCH  ="laptop";
 
 // verificar si no esta vacio.
 if (!empty($SEARCH)){
     //eliminar caract especiales para la busqueda SQL
     $buscar = mysqli_real_escape_string($conn,$SEARCH);
     //realizar la consulta SQL
-    $sql = "SELECT * FROM stock WHERE Nombre LIKE '%$SEARCH%'";
+    $sql = "SELECT * FROM stock WHERE Tipo LIKE '%$SEARCH%'";
     //preparamos la consulta.
-    $result = mysqli_query( $conn, $sql);
-    if (mysqli_num_rows($result)>0){
-        while($fila = mysqli_fetch_assoc($result)){
-            echo $fila[]
+    $consulta = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($consulta)>0){
+        $rspt = array();
+        if (mysqli_num_rows($consulta)>0){
+            while($fila = mysqli_fetch_assoc($consulta)){
+                $rspt[] = $fila;
+            }
+            echo json_encode($rspt);
         }
-    }
-    //obtener numero de filas del resultado.
-    $total = mysqli_num_rows($stmt);
-    //si hay una fila que coincida.
-    if ($total>0){
-        $datos[]=array();
-        //mandar mensaje que se encontraron resultados.
-        echo json_encode("<p>se encotraron $total de resultados para: <b>$buscar</b></p>");
-        //recorre la filas del resultado y muestra los datos buscados.
-        while($row =mysqli_fetch_array($resultado)){
-            $datos[]=array(
-                "id"=>$row['Id'],
-                "nombre"=>$row['Nombre']
-            );
-
-        }
-        //devolver arreglo com JSON.
-        echo json_encode($datos);
     }else{
-        echo json_encode(array("error"=>"no se encontraron resultados para :$buscar"));
+        $sql ="SELECT * FROM stock WHERE Nombre LIKE '%$SEARCH%'";
+        $consulta = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($consulta)>0){
+            while($fila = mysqli_fetch_assoc($consulta)){
+                $rspt[] = $fila;
+            }
+            echo json_encode($rspt);
+        }
     }
+    
+    
 }
 ?>
