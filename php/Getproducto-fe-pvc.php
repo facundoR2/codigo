a18736cb19d38f71fb49af5fb02218c31209e91b
facundoR2 <facundo_rios1@hotmail.com>
2023-10-10 12:22:20 -0300

    se modifico para que el php busque usando el nombre almacenado en el session storage y traiga todo.

--- php/Getproducto-fe-pvc.php
+++ php/Getproducto-fe-pvc.php
@@ -1,8 +1,26 @@
 <?php
 include ("conexion.php");
 session_start();
-$datosproducto = isset($_SESSION['jsonaEnviar'])?$_SESSION['jsonaEnviar']:'NO hay datos';
-echo json_encode($datosproducto);
-//destruimos la session para limpiar la informacion asociada
-//tanto en el server como del lado cliente.
+
+
+$Nombre =$_POST["Nombre"];
+// $Nombre ="Impresora HP Laserjet Pro M428fdw";
+//sanitizar el nombre para evitar inyeccion sql
+$Nombre =mysqli_real_escape_string($conn,$Nombre);
+// realizamos la busqueda en la base de datos ?
+$consulta= mysqli_query($conn, "SELECT * FROM stock WHERE Nombre='$Nombre'");
+//creamos un array para capturar los resultados (si es que hay).
+$resultado = array();
+//verificar si algun registro coincide.
+if ($consulta && mysqli_num_rows($consulta)>0){
+    while($fila =mysqli_fetch_assoc($consulta)){
+        $resultado[]= $fila;
+    }
+    $json = json_encode($resultado);
+    echo $json;
+
+}
+else{
+    echo json_encode("no se encontro nombre");
+}
 ?>
