7d6a11bbe8630d634e7513f07cb488ea5ac47ae4
facundoR2 <facundo_rios1@hotmail.com>
2023-10-12 14:54:56 -0300

    se agregaron las funciones de la adminpag(1/10)

---
+++ adminpag/dinamic.js
@@ -0,0 +1,19 @@
+// se crean los contenedores de los elementos html
+let nuevologin = document.getElementById('Bingresar');
+let ADMproducts = document.getElementById("bton-adm-Products");
+let Grafics = document.getElementById("bton-grafics-stats");
+let orders = document.getElementById("bton-adm-orders");
+
+// inicio funcionalidad de botones de  navegacion del Admin.
+
+ADMproducts.addEventListener("click",function(){
+    window.location.href="http://localhost/Neutro/codigo/adminpag/adminproduct/ADMpindex.hmtl";
+});
+Grafics.addEventListener("click",function(){
+    
+})
+orders.addEventListener("click",function(){
+
+})
+
+
