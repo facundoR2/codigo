2cdfcf192ca54319d17345e46ebf07ee65e322b2
facundoR2 <facundo_rios1@hotmail.com>
2023-10-14 00:18:37 -0300

    primer intento de agregar el select(capas anda)

--- php/phpadmin/httpProducto.php
+++ php/phpadmin/httpProducto.php
@@ -1,14 +1,28 @@
 <!-- en este php se incluiran los comandos GET;POST;UPDATE;DELETE fisico y ideal -->
-<?php 
+<?php
+include("../conexion.php");
+
 if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+    $sql = "SELECT * FROM productos";
+    
+    //preparamos la request
+    $stmt= mysqli_query($conn,$sql);
+    //ejecutamos la secuencia
+    $datos = mysqli_fetch_all($stmt,MYSQLI_ASSOC);
+    print_r($datos);
+    $json = json_encode($datos);
+    echo $json;
+    $conn->close();
+
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
 
 }
-if ($_SERVER['REQUEST_METHOD'] === 'UPDATE'){
+if ($_SERVER['REQUEST_METHOD'] === 'PUT'){
 
 }
-if ($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+if ($_SERVER['REQUEST_METHOD'] === 'Delete'){
     
 }
 
