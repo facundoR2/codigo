8cb25818f752bbfd6fef0ad5a81880ce054d2ced
facundoR2 <facundo_rios1@hotmail.com>
2023-10-13 22:40:55 -0300

    correccion de nombres indexs. se direccionaron los botones de navegacion.

--- adminpag/dinamic.js
+++ adminpag/dinamic.js
@@ -10,10 +10,10 @@ ADMproducts.addEventListener("click",function(){
     window.location.href="http://localhost/Neutro/codigo/adminpag/adminproduct/ADMpindex.hmtl";
 });
 Grafics.addEventListener("click",function(){
-    
+    window.location.href="http://localhost/Neutro/codigo/adminpag/estadisticas/stas_index.html";
 })
 orders.addEventListener("click",function(){
-
+    window.location.href="http://localhost/Neutro/codigo/adminpag/ordenes/orders_index.html";
 })
 
 
