7b3b9654e469f30a3ccac3c94c83a94509a4fee3
facundoR2 <facundo_rios1@hotmail.com>
2023-10-14 00:14:08 -0300

    primera parte de la funcion buscardatos

--- adminpag/dinamic.js
+++ adminpag/dinamic.js
@@ -7,7 +7,7 @@ let orders = document.getElementById("bton-adm-orders");
 // inicio funcionalidad de botones de  navegacion del Admin.
 
 ADMproducts.addEventListener("click",function(){
-    window.location.href="http://localhost/Neutro/codigo/adminpag/adminproduct/ADMpindex.hmtl";
+    window.location.href="http://localhost/Neutro/codigo/adminpag/adminproduct/ADMpindex.html";
 });
 Grafics.addEventListener("click",function(){
     window.location.href="http://localhost/Neutro/codigo/adminpag/estadisticas/stas_index.html";
@@ -16,4 +16,21 @@ orders.addEventListener("click",function(){
     window.location.href="http://localhost/Neutro/codigo/adminpag/ordenes/orders_index.html";
 })
 
+// ------------------- fin botones de nav ------------------//
+// -----------------funcion para solicitar los datos de las tablas---------//
+ async function obtenerinfo(){
 
+    let respuesta = await fetch ("http://localhost/Neutro/codigo/php/phpadmin/httpProducto.php");
+    if(respuesta.ok){
+        let datos = await respuesta.json();
+        let tabla = document.createElement("table");
+        let tbody = document.createElement("tbody");
+        for(let item of datos){
+            let fila = document.createElement("tr");
+            let celdaId = document.createElement("td");
+            celdaId.textContent= item.id;
+            let celdaNombre = document.createElement("td");
+            celdaNombre.textContent = item.Nombre;
+        }
+    }
+}
