7cc964923974466ffe01f4c604bfc7630804b718
facundoR2 <facundo_rios1@hotmail.com>
2023-10-14 21:20:59 -0300

    se agrego el hash al registro.php falta redireccionarlo a usuarios. ver forma de darle incremental a id carrito.

--- php/registro.php
+++ php/registro.php
@@ -3,8 +3,16 @@ include ("conexion.php");
 $mail = $_POST['Mail'];
 $contraseña = $_POST['Contraseña'];
 
-$query = "INSERT INTO info(id, Mail, Contraseña) VALUES ('NULL','$mail','$contraseña')";
-$ejecutar =mysqli_query($conn, $query);
+// se encripta la contraseña por seguridad.
+$hash = password_hash($contraseña, PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO info(id, Mail, Contraseña) VALUES ('NULL','?','?)";
+//preparamos la consulta.
+$stmt =$conn->prepare($sql);
+//binculamos los parametros con valores insertar.
+$stmt->bind_param("ss", $mail, $hash);
+//ejecutamos la consulta.
+$stmt->execute();
 
 if($ejecutar){
     echo json_encode("USUARIO REGISTRADO EXITOSAMENTE");
