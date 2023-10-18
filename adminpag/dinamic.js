d76b6c9717ba5f4435d6f632583f008620dfb776
facundoR2 <facundo_rios1@hotmail.com>
2023-10-18 15:52:56 -0300

    se agrego funcion que guarda el contenido del label en caso de pagreload. se agrego alerta de saludo con nombrede usuario.

--- adminpag/dinamic.js
+++ adminpag/dinamic.js
@@ -1,11 +1,21 @@
 // se crean los contenedores de los elementos html
+let inicio = document.getElementById("Logo-tienda");
 let nuevologin = document.getElementById('Bingresar');
 let ADMproducts = document.getElementById("bton-adm-Products");
 let Grafics = document.getElementById("bton-grafics-stats");
 let orders = document.getElementById("bton-adm-orders");
 
 // inicio funcionalidad de botones de  navegacion del Admin.
-
+inicio.addEventListener("click",function(){
+    let campousuario = document.getElementById("labelusuario");
+    if(sessionStorage.getItem("autosave")){
+        campousuario.innerHTML = sessionStorage.getItem("autosave");
+    }
+    campousuario.addEventListener("change",()=>{
+        sessionStorage.setItem("autosave",campousuario.innerHTML);
+    })
+    window.location.reload();
+});
 ADMproducts.addEventListener("click",function(){
     window.location.href="http://localhost/Neutro/codigo/adminpag/adminproduct/ADMpindex.html";
 });
@@ -34,3 +44,11 @@ orders.addEventListener("click",function(){
         }
     }
 }
+window.addEventListener("DOMContentLoaded",function(){
+    let usuario = sessionStorage.getItem("usuario:");
+    if(usuario==null || usuario==" "){
+        this.alert("usted no esta en una session");
+    }else{
+        this.alert("bienvenido",usuario);
+    }
+});
