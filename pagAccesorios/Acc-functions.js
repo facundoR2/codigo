311491c89929f4bb19f0c6eb9abf1bfcc39117a7
facundoR2 <facundo_rios1@hotmail.com>
2023-11-02 17:08:25 -0300

    se agregó funcion de traer productos y conexion a pagvistacomponente.

--- pagAccesorios/Acc-functions.js
+++ pagAccesorios/Acc-functions.js
@@ -1,14 +1,19 @@
 //se agregan los listeners para los buttons del menu lateral.
 let nuevologin = document.getElementById('Bingresar');
+let logoitem = document.getElementById("Logo-tienda");
 let BtonAccesorios = document.getElementById("bton-accesorios");
 let BtonCategorias = document.getElementById("bton-categorias");
+let Go_inicio = document.getElementById("bton-inicio");
 let Go_tocart = document.getElementById("bton-carrito");
 let Go_mypc = document.getElementById("bton-ATP");
-let Go_products = document.getElementById("bton-productos");
+
 
 // seccion para la funcionalidad de busqueda.
 let buscador = document.getElementById("buscador");
 let botonBusqueda = document.getElementById("BotonBuscar");
+Go_inicio.addEventListener("click",function(){
+    window.location.replace("http://localhost/Neutro/codigo/pagPrincipal/index.html");
+});
 logoitem.addEventListener("click",function(){
     window.location.href="http://localhost/Neutro/codigo/pagPrincipal/index.html";
 });
@@ -19,6 +24,32 @@ buscador.addEventListener("click",function(){
     console.log(item);
     buscarProducto(B_item);
 });
+//listeners de botones para navegar en las paginas.
+nuevologin.addEventListener("click",function(){
+    verificarSession()
+    if(verificarSession==true){
+        alert("ya estas en una session");
+    }else{
+        window.location.href="http://localhost/Neutro/codigo/paglogin/loginindex.html";
+    }
+});
+BtonCategorias.addEventListener("click",function(){
+    window.location.href="http://localhost/Neutro/codigo/pagCategorias/index.html";
+});
+
+Go_mypc.addEventListener("click",function(){
+    alert("esta funcion todavia no esta lista");
+    // window.location.href="http://localhost/Neutro/codigo/pagArmarTuPc/index.html";
+});
+Go_tocart.addEventListener("click",function(){
+    verificarSession()
+    if(verificarSession==true){
+        window.location.href="http://localhost/Neutro/codigo/pagCarrito/index.html";
+    }else{
+        alert("por favor ingresa a una sesion para acceder a un carrito");
+    }
+});
+
 function buscarProducto(B_item){
      var busqueda = B_item
      let searchjson = JSON.stringify(busqueda);
@@ -45,123 +76,99 @@ function buscarProducto(B_item){
 //------------------- fin seccion busqueda--------------------//.
 //--------------seccion configuraciones de session-----------//.
 function verificarSession(){
-    let usuarionombre = document.getElementById("labelusuario");
-    var usuario = usuarionombre.textContent;
-    console.log(usuario);
-    if (usuario == "cliente"){
+    //crea la variable que contiene el label
+    var nomUsuario = sessionStorage.getItem("usuario");
+    if(!nomUsuario){
         alert("no has ingresado a una session");
         window.location.href="http://localhost/neutro/codigo/paglogin/loginindex.html";
-        var respuesta = false;
-        return respuesta;
+        return false;
     }else{
-        alert("estas en una session");
-        var respuesta = true;
-        return respuesta;
+        return true;
     }
-};
-function configsession(){
-    var label = document.getElementById("labelusuario");
-    var botonsession = document.getElementById("Bingresar");
-    var usuario = sessionStorage.getItem("usuario");
-    if( label =="" || !usuario){
-        label.innerHTML="cliente";
-        
-    }else{
-        label.innerHTML = usuario;
-        botonsession.innerHTML ="cerrar session";
-    }     
-};
+}
+
 //-----------------------fin seccion session---------------------////.
-//listeners de botones para navegar en las paginas.
-nuevologin.addEventListener("click",function(){
-    verificarSession()
-    if(verificarSession==true){
-        alert("ya estas en una session");
-    }else{
-        window.location.href="http://localhost/Neutro/codigo/paglogin/loginindex.html";
-    }
-});
-BtonCategorias.addEventListener("click",function(){
-    window.location.href="http://localhost/Neutro/codigo/pagCategorias/index.html";
-});
-BtonAccesorios.addEventListener("click",function(){
-    window.location.href="http://localhost/Neutro/codigo/pagAccesorios/index.html";
-});
-Go_mypc.addEventListener("click",function(){
-    window.location.href="http://localhost/Neutro/codigo/pagArmarTuPc/index.html";
-});
-Go_tocart.addEventListener("click",function(){
-    verificarSession()
-    if(verificarSession==true){
-        window.location.href="http://localhost/Neutro/codigo/pagCarrito/index.html";
-    }else{
-        alert("por favor ingresa a una sesion para acceder a un carrito");
-    }
-});
 
 //---------------------inicio seccion traer productos-------------------//.
-function buscarProducto(B_item){
-    var busqueda = B_item;
-    let search_Fdata = new FormData();
-    search_Fdata.append("Busqueda",busqueda);
-
-   fetch("http://localhost/Neutro/codigo/php/Buscarproducto-fe-pp1.php",{
-       method: 'POST',
-       body: search_Fdata
-   })
-   .then(function(respuesta){
-       if(respuesta.ok){
-           console.log("busqueda exitosa");
-           return respuesta.json();
-       }else{
-           console.log("busqueda incompleta");
-           window.location.href="http://localhost/Neutro/codigo/pagBuscarObjeto/index.html";
-       }
-   })
-   .then(function(data){
-       console.log(data);
-       sessionStorage.setItem("busqueda",JSON.stringify(data));
-       window.location.href="http://localhost/Neutro/codigo/pagBuscarObjeto/PBOindex.html";
-   });
+function iraproducto(Nombre){
+    //verificamos que capturo el nombre.
+    console.log(Nombre);
+    //agregamos el nombre dentro de sessionStorege(solo se almacena dentro de la session hasta que se cierra o reinicia la pag).
+    sessionStorage.setItem("producto",Nombre);
+    window.location.href="http://localhost/Neutro/codigo/pagvistacomponente/PVICindex.html";
+            
 };
-function traerproducto(){
-    fetch("http://localhost/Neutro/codigo/php/Getproducto-fe-pp.php")
-    //convertimos la respuesta en un  objeto json.
-    .then(Response => Response.json())
-    .then(data=>{
-        information = data;
-        let contenedorproductos = document.getElementById("contenedor-productos");
-        for(var i=0;i<information.length;i++){
-            //creamos div para el producto
-            var producto = document.createElement("div");
-            //le asigno nombre
-            producto.className ="Producto";
-            producto.onclick=function(){
+
+
+function crearProductos(json){
+    var productos = json;
+
+    var categorias = Object.keys(productos);
+    
+    //reccorer array de claves y acceder a cada array por categoria.
+    for(var i = 0 ; i<categorias.length;i++){
+        //obtener id de categoria actual
+        var id_categoria = categorias[i];
+
+        //obtener el array de productos de categoria actual
+        var productos_categoria = productos[id_categoria];
+
+        //obtener contenedor de categoria por su id
+        var contenedor = document.getElementById(id_categoria);
+        
+
+        //recorrer el array de productos 
+        for (var j = 0; j < productos_categoria.length; j++) {
+            // Obtener el producto actual
+            var producto = productos_categoria [j];
+
+            // Crear un elemento div para contener la información del producto
+            var div = document.createElement ("div");
+            div.className="Producto";
+            div.onclick =function(){
                 var h3 = this.querySelector("h3");
                 var nom = h3.textContent;
                 console.log("comprarste el producto: " + nom);
                 iraproducto(nom);
-            }
-            var nombre = document.createElement("h3");
-            nombre.className="Newsproducts";
-            nombre.textContent = information[i].Nombre;
-            //creo y le asigno una imagen
-            var img = document.createElement("img");
-            img.alt="no hay imagen disponible.";
-            img.src = information[i].Imagen;
-            //creamos y asignamos un parrafo para el precio.
-            var precio  = document.createElement("p");
-            precio.className="Product-p";
-            precio.textContent = "$"+ information[i].Costo;
-            //agregamos los items al contenedor "producto".
-            producto.appendChild(img);
-            producto.appendChild(nombre);
-            producto.appendChild(precio);
-            //agregamos el producto al contenedor de productos.
-            contenedorproductos.appendChild(producto);
-        };
+            };
+
+            // Crear un elemento h3 para mostrar el nombre del producto
+            var h3 = document.createElement ("h3");
+            h3.textContent = producto ["Nombre"];
+
+            // Crear un elemento p para mostrar el precio del producto
+            var p = document.createElement ("p");
+            p.textContent = "$" + producto ["Costo"];
+            // Crear un elemento img para mostrar la imagen del producto
+            var img = document.createElement ("img");
+            img.setAttribute ("src", producto ["imagen"]);
+            img.setAttribute ("alt", "no se encontro imagen");
+
+            // Añadir los elementos al div
+            div.appendChild (img);
+            div.appendChild (h3);
+            div.appendChild (p);
+            // Añadir el div al contenedor de categoría
+            contenedor.appendChild (div);
+        }
+    }
+}
+
+
+function traerproducto(){
+    fetch("http://localhost/Neutro/codigo/php/Get_categorias.php")
+    //convertimos la respuesta en un  objeto json.
+    .then(function(response){
+        if(response.ok){
+            return response.json();
+        }
+    })
+    .then(function(data){
+        console.log(data);
+        crearProductos(data)
     });
-};
+}
+
 
 //-------------------------seccion cookies-------------------------------//.
 function crearCookie(nombre, valor, dias){
@@ -186,8 +193,19 @@ function obtenerCookie(nombre) {
     return"";
 }
 //------------------fin seccion cookies--------------------//.
+function config_status_session(){
+    var label = document.getElementById("labelusuario");
+    var botonsession = document.getElementById("Bingresar");
+    var usuario = sessionStorage.getItem("usuario");
+    if( label =="" || !usuario){
+        label.innerHTML="cliente";
+        
+    }else{
+        label.innerHTML = usuario;
+        botonsession.innerHTML ="cerrar session";
+    }     
+};
 window.addEventListener("DOMContentLoaded",function(){
-    confirmarCookies();
-    // traerproducto();
-    configsession();
-});
+    traerproducto();
+    config_status_session();
+})
